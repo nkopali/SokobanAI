1a17f3a998fe1c0c092e99a9be5543a16084fc67
Nils <35413831+nkopali@users.noreply.github.com>
2022-02-27 19:52:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,281 @@
+import numpy as np
+import heapq
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import chromedriver_autoinstaller
+
+class PriorityQueue:
+    """Define a PriorityQueue data structure that will be used"""
+    def  __init__(self):
+        self.Heap = []
+        self.Count = 0
+
+    def push(self, item, priority):
+        entry = (priority, self.Count, item)
+        heapq.heappush(self.Heap, entry)
+        self.Count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.Heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.Heap) == 0
+
+"""Load puzzles and define the rules of sokoban"""
+
+def transferToGameState(layout):
+    """Transfer the layout of initial puzzle"""
+    layout = [x.replace('\n','') for x in layout]
+    layout = [','.join(layout[i]) for i in range(len(layout))]
+    layout = [x.split(',') for x in layout]
+    maxColsNum = max([len(x) for x in layout])
+    for irow in range(len(layout)):
+        for icol in range(len(layout[irow])):
+            if layout[irow][icol] == ' ': layout[irow][icol] = 0   # free space
+            elif layout[irow][icol] == '#': layout[irow][icol] = 1 # wall
+            elif layout[irow][icol] == '&': layout[irow][icol] = 2 # player
+            elif layout[irow][icol] == 'B': layout[irow][icol] = 3 # box
+            elif layout[irow][icol] == '.': layout[irow][icol] = 4 # goal
+            elif layout[irow][icol] == 'X': layout[irow][icol] = 5 # box on goal
+        colsNum = len(layout[irow])
+        if colsNum < maxColsNum:
+            layout[irow].extend([1 for _ in range(maxColsNum-colsNum)]) 
+    return np.array(layout)
+
+def PosOfPlayer(gameState):
+    """Return the position of agent"""
+    return tuple(np.argwhere(gameState == 2)[0]) # e.g. (2, 2)
+
+def PosOfBoxes(gameState):
+    """Return the positions of boxes"""
+    return tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5))) # e.g. ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5))
+
+def PosOfWalls(gameState):
+    """Return the positions of walls"""
+    return tuple(tuple(x) for x in np.argwhere(gameState == 1)) # e.g. like those above
+
+def PosOfGoals(gameState):
+    """Return the positions of goals"""
+    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5))) # e.g. like those above
+
+def isEndState(posBox):
+    """Check if all boxes are on the goals (i.e. pass the game)"""
+    return sorted(posBox) == sorted(posGoals)
+
+def isLegalAction(action, posPlayer, posBox):
+    """Check if the given action is legal"""
+    xPlayer, yPlayer = posPlayer
+    if action[-1].isupper(): # the move was a push
+        x1, y1 = xPlayer + 2 * action[0], yPlayer + 2 * action[1]
+    else:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+    return (x1, y1) not in posBox + posWalls
+
+def legalActions(posPlayer, posBox):
+    """Return all legal actions for the agent in the current game state"""
+    allActions = [[-1,0,'u','U'],[1,0,'d','D'],[0,-1,'l','L'],[0,1,'r','R']]
+    xPlayer, yPlayer = posPlayer
+    legalActions = []
+    for action in allActions:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+        if (x1, y1) in posBox: # the move was a push
+            action.pop(2) # drop the little letter
+        else:
+            action.pop(3) # drop the upper letter
+        if isLegalAction(action, posPlayer, posBox):
+            legalActions.append(action)
+        else: 
+            continue     
+    return tuple(tuple(x) for x in legalActions) # e.g. ((0, -1, 'l'), (0, 1, 'R'))
+
+def updateState(posPlayer, posBox, action):
+    """Return updated game state after an action is taken"""
+    xPlayer, yPlayer = posPlayer # the previous position of player
+    newPosPlayer = [xPlayer + action[0], yPlayer + action[1]] # the current position of player
+    posBox = [list(x) for x in posBox]
+    if action[-1].isupper(): # if pushing, update the position of box
+        posBox.remove(newPosPlayer)
+        posBox.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1]])
+    posBox = tuple(tuple(x) for x in posBox)
+    newPosPlayer = tuple(newPosPlayer)
+    return newPosPlayer, posBox
+
+def isFailed(posBox):
+    """This function used to observe if the state is potentially failed, then prune the search"""
+    rotatePattern = [[0,1,2,3,4,5,6,7,8],
+                    [2,5,8,1,4,7,0,3,6],
+                    [0,1,2,3,4,5,6,7,8][::-1],
+                    [2,5,8,1,4,7,0,3,6][::-1]]
+    flipPattern = [[2,1,0,5,4,3,8,7,6],
+                    [0,3,6,1,4,7,2,5,8],
+                    [2,1,0,5,4,3,8,7,6][::-1],
+                    [0,3,6,1,4,7,2,5,8][::-1]]
+    allPattern = rotatePattern + flipPattern
+
+    for box in posBox:
+        if box not in posGoals:
+            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1), 
+                    (box[0], box[1] - 1), (box[0], box[1]), (box[0], box[1] + 1), 
+                    (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
+            for pattern in allPattern:
+                newBoard = [board[i] for i in pattern]
+                if newBoard[1] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posBox: return True
+                elif newBoard[1] in posBox and newBoard[2] in posBox and newBoard[5] in posBox: return True
+                elif newBoard[1] in posBox and newBoard[6] in posBox and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls: return True
+    return False
+
+def heuristic(posPlayer, posBox):
+    """A heuristic function to calculate the overall distance between the else boxes and the else goals"""
+    distance = 0
+    completes = set(posGoals) & set(posBox)
+    sortposBox = list(set(posBox).difference(completes))
+    sortposGoals = list(set(posGoals).difference(completes))
+    for i in range(len(sortposBox)):
+        distance += (abs(sortposBox[i][0] - sortposGoals[i][0])) + (abs(sortposBox[i][1] - sortposGoals[i][1]))
+    return distance
+
+def cost(actions):
+    """A cost function"""
+    return len([x for x in actions if x.islower()])
+
+def aStarSearch():
+    """Implement aStarSearch approach"""
+    beginBox = PosOfBoxes(gameState)
+    beginPlayer = PosOfPlayer(gameState)
+
+    start_state = (beginPlayer, beginBox)
+    frontier = PriorityQueue()
+    frontier.push([start_state], heuristic(beginPlayer, beginBox))
+    exploredSet = set()
+    actions = PriorityQueue()
+    actions.push([0], heuristic(beginPlayer, start_state[1]))
+    while frontier:
+        node = frontier.pop()
+        node_action = actions.pop()
+        if isEndState(node[-1][-1]):
+            result = ','.join(node_action[1:]).replace(',','')
+            print("Results:"+result)
+            return result
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            Cost = cost(node_action[1:])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                Heuristic = heuristic(newPosPlayer, newPosBox)
+                frontier.push(node + [(newPosPlayer, newPosBox)], Heuristic + Cost) 
+                actions.push(node_action + [action[-1]], Heuristic + Cost)
+
+def uniformCostSearch():
+    """Implement uniformCostSearch approach"""
+    beginBox = PosOfBoxes(gameState)
+    beginPlayer = PosOfPlayer(gameState)
+
+    startState = (beginPlayer, beginBox)
+    frontier = PriorityQueue()
+    frontier.push([startState], 0)
+    exploredSet = set()
+    actions = PriorityQueue()
+    actions.push([0], 0)
+    while frontier:
+        node = frontier.pop()
+        node_action = actions.pop()
+        if isEndState(node[-1][-1]):
+            result = ','.join(node_action[1:]).replace(',','')
+            print("Results: "+result)
+            return result
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            Cost = cost(node_action[1:])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                frontier.push(node + [(newPosPlayer, newPosBox)], Cost)
+                actions.push(node_action + [action[-1]], Cost)
+
+def mapDirectionToKey(direction):
+        if direction == 'u': return Keys.ARROW_UP
+        elif direction == 'd': return Keys.ARROW_DOWN
+        elif direction == 'l': return Keys.ARROW_LEFT
+        elif direction == 'r': return Keys.ARROW_RIGHT
+        else: raise Exception('Invalid direction!')
+    
+
+def online_try(res):
+    
+    solutionKeys = list(map(mapDirectionToKey, res.lower()))
+
+    url = "https://www.sokobanonline.com/play/web-archive/marti-homs-caussa/choriban/86889_choriban-22"
+
+    driverPath = chromedriver_autoinstaller.install()
+    service = Service(driverPath)
+    options = webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(url)
+
+    # Wait until JavaScript loads and canvas appears
+    canvas = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.TAG_NAME, 'canvas')))
+
+    # Set up ActionChains for keyboard key sends
+    actions = ActionChains(driver)
+
+    for key in solutionKeys:
+        actions.send_keys(key)
+        actions.perform()
+        time.sleep(0.1)
+
+    input("Press enter to End")
+
+if __name__ == '__main__':
+    filepath = os.path.abspath(os.getcwd()) + "\level1.txt"
+
+    with open(filepath,"r") as f:
+        layout = f.readlines()
+
+    gameState = transferToGameState(layout)
+    posWalls = PosOfWalls(gameState)
+    posGoals = PosOfGoals(gameState)
+
+    print("Press 1 for Uniform Cost Search.")
+    print("Press 2 for A star Search.")
+
+    selection = int(input("Choice: "))
+
+    if selection == 1:
+        time_start = time.time()
+
+        res = uniformCostSearch()
+
+        time_end=time.time()
+        print('Runtime of Uniform Search: %.2f second.' %( time_end-time_start))
+        online_try(res)
+    elif selection == 2:
+        time_start = time.time()
+
+        res = aStarSearch()
+
+        time_end=time.time()
+        print('Runtime of A* Search: %.2f second.' %( time_end-time_start))
+        online_try(res)
+    else:
+        print("Wrong command")
+
+
+    
